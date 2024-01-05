1cccc8c5f770028fe194abce2b4a8c1c02be6ae8
pwhb <pwhbdev@gmail.com>
2024-01-05 23:35:57 +0630

    remove unused

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { useEffect } from 'react';
 import './App.css';
 import { auth } from './lib/firebase';
 import Navbar from './lib/components/Navbar';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setUser } from './lib/redux/authSlice';
 import CockLauncher from './lib/components/CockLauncher';
 
 function App()
 {
-  const { currentUser } = useSelector((state: any) => state.auth);
-
   const dispatch = useDispatch();
 
   useEffect(() =>
@@ -25,8 +23,6 @@ function App()
     <>
       <div className='h-[100vh] max-w-xl mx-auto'>
         <Navbar />
-      
-        {/* <img src={VITE_SVG} alt="penis" className={`mx-auto w-20 animate-bounce`} /> */}
         <CockLauncher />
       </div>
     </>
--- src/lib/components/CockLauncher.tsx
+++ src/lib/components/CockLauncher.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
-import { PENIS_SVG, VITE_SVG } from "../consts/assets";
+import { PENIS_SVG } from "../consts/assets";
 
 export default function CockLauncher()
 {
